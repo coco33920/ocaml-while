ca592ec48a636ed843ab80c9d9a5b82bf2d328a0
Charlotte Thomas <contact@nwa2coco.fr>
2023-02-10 13:18:57 +0100

    lexer is still broken

--- lib/lexing/lexer.ml
+++ lib/lexing/lexer.ml
@@ -10,10 +10,10 @@ let lex str =
     let a,b = is_a_token (word) in
     match lst with
       | [] -> acc
-      | '('::t -> if b then aux (a::Token.OpenParenthesis::acc) "" t else aux (Token.OpenParenthesis::acc) "" t
-      | ')'::t -> if b then aux (a::Token.CloseParenthesis::acc) "" t else aux (Token.CloseParenthesis::acc) "" t
-      | '"'::t -> if b then aux (a::Token.Quote::acc) "" t else aux (Token.Quote::acc) "" t
-      | '%'::t -> if b then aux (a::Token.Percentages::acc) "" t else aux (Token.Percentages::acc) "" t
-      | ' '::t -> if b then aux (a::acc) "" t else aux acc "" t
+      | '('::t -> if b then aux (a::Token.OpenParenthesis::acc) "" t else aux (Token.OpenParenthesis::acc) word t
+      | ')'::t -> if b then aux (a::Token.CloseParenthesis::acc) "" t else aux (Token.CloseParenthesis::acc) word t
+      | '"'::t -> if b then aux (a::Token.Quote::acc) "" t else aux (Token.Quote::acc) word t
+      | '%'::t -> if b then aux (a::Token.Percentages::acc) "" t else aux (Token.Percentages::acc) word t
+      | ' '::t -> if b then aux (a::acc) "" t else if word = "" then aux acc word t else aux (Token.String word::acc) "" t
       | h::t -> let a,b = is_a_token (word^(String.make 1 h)) in if b then aux (a::acc) "" t else aux acc (word^(String.make 1 h)) t
   in List.rev (aux [] "" l_seq)
