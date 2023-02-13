f555a9eb8c0adc69dd7b6252912ecb79d384e927
Charlotte Thomas <chthomas@sta-ckk15r2.irisa.fr>
2023-02-13 14:36:02 +0100

    work in progress on lexer

--- lib/lexing/lexer.ml
+++ lib/lexing/lexer.ml
@@ -7,13 +7,12 @@ let is_a_token s =
 let lex str = 
   let l_seq = List.of_seq (String.to_seq str) in
   let rec aux acc word lst = 
-    let a,b = is_a_token (word) in
+    print_endline word;
     match lst with
       | [] -> acc
-      | '('::t -> if b then aux (a::Token.OpenParenthesis::acc) "" t else aux (Token.OpenParenthesis::acc) word t
-      | ')'::t -> if b then aux (a::Token.CloseParenthesis::acc) "" t else aux (Token.CloseParenthesis::acc) word t
-      | '"'::t -> if b then aux (a::Token.Quote::acc) "" t else aux (Token.Quote::acc) word t
-      | '%'::t -> if b then aux (a::Token.Percentages::acc) "" t else aux (Token.Percentages::acc) word t
-      | ' '::t -> if b then aux (a::acc) "" t else if word = "" then aux acc word t else aux (Token.String word::acc) "" t
+      | '('::t -> aux (Token.OpenParenthesis::acc) "" t
+      | ')'::t -> aux (Token.CloseParenthesis::acc) "" t
+      | '"'::t -> aux (Token.Quote::acc) "" t
+      | '%'::t -> aux (Token.Percentages::acc) "" t
       | h::t -> let a,b = is_a_token (word^(String.make 1 h)) in if b then aux (a::acc) "" t else aux acc (word^(String.make 1 h)) t
   in List.rev (aux [] "" l_seq)
