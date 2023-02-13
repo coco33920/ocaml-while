247129deb8e791c1d11657680c4ff45465a4e484
Charlotte Thomas <charlotte.thomas@inria.fr>
2023-02-13 15:04:24 +0100

    lexer finished with example test.while

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,7 @@
-let () = print_endline "Hello, World!"
-
 let sd =
   Ocaml_while.Utils.read_file "examples/test.while"
   |> String.concat " " |> Ocaml_while.Lexer.lex
 ;;
 
-List.map (fun c -> print_string (Ocaml_while.Token.print_token c)) sd
+List.map (fun c -> print_string (Ocaml_while.Token.print_token c)) sd;;
+print_newline;;
